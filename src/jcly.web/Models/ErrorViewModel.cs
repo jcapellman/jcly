2e71bb1145f7419cb9cd661a612a24c173fd960e
Jarred Capellman <jcapellman@hotmail.com>
2020-06-20 16:21:59 -0500

    Added: Initial checkin

---
+++ src/jcly.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jcly.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
