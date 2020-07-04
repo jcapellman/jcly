00e5a7292fcc78201689fb4f8730b57812df9927
Jarred Capellman <jcapellman@hotmail.com>
2020-07-03 22:03:32 -0500

    Added: Initial work on the BaseDAL class

---
+++ src/jcly.lib/DAL/Base/BaseDAL.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace jcly.lib.DAL.Base
+{
+    public abstract class BaseDAL
+    {
+        public abstract Task<string> InsertURLAsync(string url);
+
+        public abstract Task<string> GetURLAsync(string key);
+    }
+}
