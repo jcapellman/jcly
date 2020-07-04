1ca2b56fe17ce253fc1379a927f36c408005a6d2
Jarred Capellman <jcapellman@hotmail.com>
2020-07-04 09:52:56 -0500

    Added: Initial work on the Generated page

---
+++ src/jcly.web/Pages/Generated.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace jcly.web.Pages
+{
+    public class GeneratedModel : PageModel
+    {
+        private readonly ILogger<GeneratedModel> _logger;
+
+        [BindProperty(SupportsGet = true)]
+        public string Key { get; set; }
+
+        public GeneratedModel(ILogger<GeneratedModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (string.IsNullOrEmpty(Key))
+            {
+                return RedirectToPage("./Error");
+            }
+
+            Key = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}/{Key}";
+
+            return Page();
+        }
+    }
+}
