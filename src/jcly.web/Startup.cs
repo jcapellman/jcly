031707028a62af7aa62ac4807dbcd2fb3c8c0505
Jarred Capellman <jcapellman@hotmail.com>
2020-07-04 09:02:36 -0500

    Added: DB to the Startup class

--- src/jcly.web/Startup.cs
+++ src/jcly.web/Startup.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using jcly.lib.DAL;
+using jcly.lib.DAL.Base;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,13 +18,13 @@ namespace jcly.web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddSingleton<BaseDAL>(new LitedbDal());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,8 +39,6 @@ namespace jcly.web
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
