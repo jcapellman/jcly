53c72b977fcdad09744ccaa9f24ca699371e8757
Jarred Capellman <jcapellman@hotmail.com>
2020-07-04 09:53:37 -0500

    Added: Exception Page changes

--- src/jcly.web/Startup.cs
+++ src/jcly.web/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace jcly.web
 {
@@ -27,15 +26,8 @@ namespace jcly.web
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
+            app.UseDeveloperExceptionPage();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
