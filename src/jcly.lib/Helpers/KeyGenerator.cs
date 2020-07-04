3aa8e23d0534b738da9f67fdb8558fb2ba9dbe29
Jarred Capellman <jcapellman@hotmail.com>
2020-07-04 08:56:18 -0500

    Added: Initial work on the KeyGenerator class

---
+++ src/jcly.lib/Helpers/KeyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace jcly.lib.Helpers
+{
+    public static class KeyGenerator
+    {
+        private static readonly Random random = new Random();
+
+        private const string KEY_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        public static string Generate(int length = 8) =>
+            new string(Enumerable.Repeat(KEY_CHARACTERS, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
