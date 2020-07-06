21833bc378b7ec0a42682afba6d36dda0e20baf1
Jarred Capellman <jcapellman@hotmail.com>
2020-07-05 19:40:40 -0500

    Change: Swapped the magic number for a constant

--- src/jcly.lib/Helpers/KeyGenerator.cs
+++ src/jcly.lib/Helpers/KeyGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
 
+using jcly.lib.Common;
+
 namespace jcly.lib.Helpers
 {
     public static class KeyGenerator
@@ -9,7 +11,7 @@ namespace jcly.lib.Helpers
 
         private const string KEY_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-        public static string Generate(int length = 8) =>
+        public static string Generate(int length = Constants.KEY_LENGTH) =>
             new string(Enumerable.Repeat(KEY_CHARACTERS, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
     }
