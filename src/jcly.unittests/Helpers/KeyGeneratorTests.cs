aad5628f8eab595aea2feeaf009784e072430d86
Jarred Capellman <jcapellman@hotmail.com>
2020-07-15 19:04:55 -0500

    Added: KeyGeneratorTests class

---
+++ src/jcly.unittests/Helpers/KeyGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using jcly.lib.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jcly.unittests.Helpers
+{
+    [TestClass]
+    public class KeyGeneratorTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeTest()
+        {
+            KeyGenerator.Generate(-1);
+        }
+    }
+}
